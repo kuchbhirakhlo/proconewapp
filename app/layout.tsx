6515813182e5030b61b27761a1d1bb8fb6774e48
Aviral Shukla <aviralshukla@Avirals-Air.lan>
2026-01-18 14:53:14 +0530

    googletags

--- app/layout.tsx
+++ app/layout.tsx
@@ -167,15 +167,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-7LMD5WF7XW"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-7LMD5WF7XW');
-</script>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7LMD5WF7XW"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-7LMD5WF7XW');
+            `,
+          }}
+        />
         {/* ✅ Google AdSense Script */}
         <Script
           id="google-adsense-script"
